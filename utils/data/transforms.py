f7737ab484c6a3358d97b95051ee5fadcdde5505
wogur110 <wogur110@gmail.com>
2022-04-20 09:04:42 +0000

    apply fivecrop for improving anomaly of edge area

--- utils/data/transforms.py
+++ utils/data/transforms.py
@@ -16,7 +16,6 @@ def Transform(load_size, input_size) :
     transform = transforms.Compose([
                         transforms.Resize((load_size, load_size), Image.ANTIALIAS),
                         transforms.ToTensor(),
-                        transforms.CenterCrop(input_size),
                         transforms.Normalize(mean=mean_train,
                                             std=std_train),
                         transforms.FiveCrop(input_size),
